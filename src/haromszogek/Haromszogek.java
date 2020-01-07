031336670d96756c6c0f17498e58e50732be75dc
Diak <Diak@10.4.18.1>
2020-01-07 17:24:36 +0100

    [Test1]: actually not tested lol

--- src/haromszogek/Haromszogek.java
+++ src/haromszogek/Haromszogek.java
@@ -1,7 +1,12 @@
 package haromszogek;
+import logika.DHaromszog;
 public class Haromszogek {
     public static void main(String[] args) {
-
+        try {
+            DHaromszog d1 = new DHaromszog("3 4 5", 1);
+            DHaromszog d2 = new DHaromszog("1,1 2,2 4,5", 2);
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+        }
     }
-    
 }
